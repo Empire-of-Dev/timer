1d2f8a9354fd22621d14cfc34c5e8de193100643
thanawin srisuk <6210612724@student.tu.ac.th>
2022-11-10 18:30:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react'
+import Countdown from "react-countdown";
 
 function App() {
+  
+  const [btnactive,setBtnActive] = useState(false)
+  
+  
+  function timer(){
+    setBtnActive(true)
+  }
+  const Completionist = () => <span>You are good to go!</span>;
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return <span className='number'>{minutes} : {seconds}</span>;
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <div className='logo-container'>
+            <img  className='logo-img' src='/logo.png' alt='logo' />           
+        </div>
+        <div className='metto'>
+          Education is a profitable  investment offering.
+        </div>
+      </div>
+      <div className='timer'>
+
+            {btnactive === false && 
+              <div className='clock'>
+              <button onClick={timer} className='btn'>Turn on Relax Mode </button>
+              </div>
+            }
+
+            {btnactive === true && 
+              <div className='clock'>
+                <button onClick={() => setBtnActive(true)} className='btn-active'>
+                    <Countdown date={Date.now() + 300000} renderer={renderer} />
+
+                </button>
+              </div>
+            }   
+     
+      </div>
     </div>
   );
 }
